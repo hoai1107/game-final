50421f1c7e1629894d919be891d29eb4f7b5c1f2
Thai Vo Duc Trong <70931622+dtrongtvo@users.noreply.github.com>
2023-09-06 00:42:00 +0700

    game complete

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision colInfo)
+    {
+         if (colInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+            
+        }
+    }
+}
