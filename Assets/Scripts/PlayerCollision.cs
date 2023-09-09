5c3aab564c3ebb24fad56538c70471c9fff0075b
hoai1107 <khaihoai1107@gmail.com>
2023-09-09 16:00:17 +0700

    Add sound effect

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerMovement movement;
+    public AudioSource collisionSoundEffect;
+
     void OnCollisionEnter(Collision colInfo)
     {
         if (colInfo.collider.tag == "Obstacle")
         {
             movement.enabled = false;
+            collisionSoundEffect.time = 0.6f;
+            collisionSoundEffect.Play();
             FindObjectOfType<GameManager>().EndGame();
         }
     }
